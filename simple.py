e1cb2c8f899ad7401a3b27d9435223e2ad2dd35f
sammy owen <ourcityourmayor@gmail.com>
2018-06-15 13:11:25 -0700

    additions to simple.py
    have it start separating out SAF's bc there are 16 of these in english, which
    maps well to 16 notes on the chromatic scale.
    
    also has an additional not about how set math on language classification could
    be interesting

--- simple.py
+++ simple.py
@@ -8,12 +8,12 @@ Usage:
 """
 
 import re
-import string
 
 
-notes = ['C','C#','Db','D','D#','Eb','F','F#','Gb','G','G#','Ab','A','A#','Bb','B']
-alphas_to_notes = dict(zip(notes, string.ascii_lowercase))
-
+notes = ['C','C#','Db','D','D#','Eb','F','F#',
+         'Gb','G','G#','Ab','A','A#','Bb','B']
+stops_affricates_fricatives = list('bcdfghjkpqsstvzz')
+alphas_to_notes = dict(zip(stops_affricates_fricatives, notes))
 
 def get_phrase():
     """Prompt the user to enter a phrase
@@ -30,43 +30,52 @@ def get_phrase():
 
 def process_phrase(phrase):
     """Remove all characters that aren't [A-Za-z] from a phrase, lc that,
-split the phrase into consonants and vowels.
+split the phrase into consonants and vowels and stops, affricates, and
+fricatives.
 
     Args:
         Phrase to be processed.
 
     Returns:
-        Tuple of vowels and consonants for unpacking.
+        Tuple of vowels and consonants and safs for unpacking.
     """
     cleaned = re.sub(r'[^A-Za-z]', '', phrase).lower()
 
     vowels = []
     consonants = []
+    safs = []
     for x in list(cleaned):
-        if re.search(r'[aeiouy]', x):
+        if re.search(r'[aeiou]', x):
             vowels.append(x)
         else:
             consonants.append(x)
 
-    return vowels, consonants
+        if x in stops_affricates_fricatives:
+            safs.append(x)
+
+    return vowels, consonants, safs
 
 
-def print_phrase(phrase):
-    """Prints the vowels and consonants of a phrase to STDOUT.
+def print_parts(phrase):
+    """Prints the vowels and consonants and SAF's of a phrase.
 
     Args:
         Phrase to be printed
     """
-    vowels, consonants = process_phrase(phrase)
+    vowels, consonants, safs = process_phrase(phrase)
     print("vowels are: {}".format(vowels))
     print("consonants are: {}".format(consonants))
+    print("saf's are: {}".format(safs))
+
+def safs_to_notes(safs):
+    pass
 
 def main():
     """Take a string and transorm it into a series of musical notes.
 
     """
     phrase = get_phrase()
-    print_phrase(phrase)
+    print_parts(phrase)
 
 
 if __name__ == '__main__':
